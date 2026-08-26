4ff0cfe7a94878c1d1cd304325e7b4f8902e8668
Tommaso Vaccari <129394032+T-vaccari@users.noreply.github.com>
2026-08-26 22:13:57 +0200

    Green test and obtained a version that runs in 0.2 seconds

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -47,44 +47,79 @@ def _merge_local_pre_token_counts(results):
    return pre_token_count
 
 
-def _pair_statistic(pre_token_count):
-   pair_count = dict()
-   for pre_token in pre_token_count.keys(): #Iterate along all the keys
-            for i in range(len(pre_token)-1):
-               a = pre_token[i]
-               b = pre_token[i+1]
-               pair = (a,b)
-               pair_count[pair]  = pair_count.get(pair, 0) + 1 * pre_token_count[pre_token]
-   return pair_count
-
 
 def _return_top_pair(pair_count):
    return max(pair_count, key = lambda  x : (pair_count[x], x))
 
-
-def _update_token_count(pre_token_count, top_pair):
-   new_pre_token_count = dict()
-
+def _initialize_pair_statistics(pre_token_count):
+   pair_count = dict()
+   pair_to_key = dict()
    for pre_token in pre_token_count.keys(): #Iterate along all the keys
-      # I have to modify the keys, so now I find that a key has the top_pair in it, I must substitue the tuple, removing
-      # the old separate entries for a and b and obtaining (old bytes*, b'top_pair', old_bytes*)
+      for i in range(len(pre_token)-1):
+         a = pre_token[i]
+         b = pre_token[i+1]
+         pair = (a,b)
+         pair_count[pair]  = pair_count.get(pair, 0) + 1 * pre_token_count[pre_token]
+         pair_to_key.setdefault(pair, set()).add(pre_token)
+   return [pair_count, pair_to_key]
+
+
+# Hot-Spot to optimize------
+# Scenario : I have found a top pair , i have decided to merge it, now i have to update the token-count, that
+# means changing the keys that contains that couple, 
+# Key-Idea : For each couple maintain a set of tuple that are the key affected by that couple
+# When updating the key of token count we can also update the statistic without rebuilding them from scratch every time
+
+
+
+
+def _update_token_count(pre_token_count, top_pair, pair_count, pair_to_key):
+   # This function is called after a new merge pair has been chose, the goal now is to do the following thing given the top pair:
+   # 1)Exploit the pair to key to find the keys affected by that pair
+   # 2)Remove the stats of the pre-toke from the pair count, modify the key taking in consideration the new pair, re-update the statistics and then
+   # I have to update the pair to key, inserting that the new created pair have that key into the pair_to_key and remove the old key from all the old couples
+   # 3) Do it for every key
+   # 4) Remove the old key from pair to key
+
+   affected_keys = list(pair_to_key[top_pair])
+
+   for key_to_update in affected_keys: #Iterate along the key to update
+      # Firs step, for each pair in the ex-key remove from the pair_to_key mapping, so i can remove the mapping pair -> key
+      # Second step, remove the stats of that pair from the dict
+      # Step 3 update the key, insert it into the dict taking in consideration the old appearances, iterate on it to update also the pair_to_key struct
+      for i in range(len(key_to_update) - 1):
+         a = key_to_update[i]
+         b = key_to_update[i+1]
+         pair = (a, b)
+         pair_to_key[pair].discard(key_to_update) #Step 1
+         pair_count[pair] -= 1 * pre_token_count[key_to_update] #Step 2
+
       merged_pre_token = list()
       i = 0
+      pre_token = key_to_update
       while i < len(pre_token):
-         if i < len(pre_token) -1 and top_pair[0]+ top_pair[1] == pre_token[i] + pre_token[i+1]:
+         if i < len(pre_token) -1 and top_pair[0] == pre_token[i] and top_pair[1] == pre_token[i + 1]:
             merged_pre_token.append(top_pair[0] + top_pair[1])
             i+=2
          else:
             merged_pre_token.append(pre_token[i])
             i+=1
 
-      merged_pre_token = tuple(merged_pre_token)
-      # print(pre_token,merged_pre_token,top_pair)
-      new_pre_token_count[merged_pre_token] = pre_token_count[pre_token]
+      updated_key = tuple(merged_pre_token)
 
-   return new_pre_token_count
+      pre_token_count[updated_key] = pre_token_count[key_to_update]
+      pre_token_count.pop(key_to_update, None)
+
+      # Now i have to update the stats with the new pair and also update the ds
+      for i in range(len(updated_key)-1):
+         a = updated_key[i]
+         b = updated_key[i+1]
+         pair = (a,b)
+         pair_count[pair]  = pair_count.get(pair, 0) + 1 * pre_token_count[updated_key]
+         pair_to_key.setdefault(pair, set()).add(updated_key)
 
 
+#---------
 
 def train_bpe(
 input_path: str | os.PathLike[str],
@@ -127,14 +162,15 @@ special_tokens: list[str]
 
    pre_token_count = _merge_local_pre_token_counts(results)
 
+   # pair_to_key should contain the mapping that goes from a given pair to the set of tuples that contains 
+   # the byte of the pre-token affected by the pair
+   pair_count , pair_to_key = _initialize_pair_statistics(pre_token_count)
 
    while len(vocab) < (vocab_size - len(special_tokens) ):
 
       
       # Now i have the pre-token dictionary, that takes into account of every chunk, now I can start to count the byte pairs and simply multiply
       # by the occurences of that precise pre-token
-
-      pair_count: dict[tuple[bytes, bytes], int] = _pair_statistic(pre_token_count)
 
       # Now I can pick-up the most frequent pair, if we have a tie I must choose the lexicographically greater pair
       
@@ -156,12 +192,11 @@ special_tokens: list[str]
 
       # Now I have to substitute each occuren of the top pair with in the tuple with the fused byte sequence
 
+      _update_token_count(pre_token_count, top_pair, pair_count, pair_to_key)
 
 
-      pre_token_count = _update_token_count(pre_token_count, top_pair)
-
-
-      # Now I have to recompute stats for the next merge round, and repeat this process until the vocab size length desired is not reached
+      # Now I have to recompute stats for the next merge round, 
+      # and repeat this process until the vocab size length desired is not reached
 
    # At the end I can insert in the vocab the special tokens
    for st in special_tokens:
@@ -188,3 +223,5 @@ if __name__ == "__main__":
         ["<|endoftext|>"],
    )
    print(vocab, merges)
+
+
